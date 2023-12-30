fcfc4a80fd6d0059178920ebabb5214e1cb084e7
wasdqwerfzxcv <72960153+wasdqwerfzxcv@users.noreply.github.com>
2023-12-31 04:02:01 +0800

    Add searchable dropdown at staff name

--- frontend/src/screens/StaffSalary.js
+++ frontend/src/screens/StaffSalary.js
@@ -1,48 +1,78 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import Loader from '../components/Loader'
-import Message from '../components/Message'
-import { PaySalary } from '../actions/staffActions'
-import { STAFF_SALARY_RESET } from '../constants/staffConstants'
-import './Student.css'
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { listStaffs, PaySalary } from '../actions/staffActions';
+import { STAFF_SALARY_RESET } from '../constants/staffConstants';
+import { Button, Input } from "@chakra-ui/react";
+import Select from 'react-select';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import './Student.css';
+
 const StaffSalary = ({ history }) => {
-  const [staffname, setStaffname] = useState('')
-  const [id, setId] = useState('')
-  const [valid, setValid] = useState(false)
-  const [year, setYear] = useState('')
-  const [salary, setSalary] = useState('')
-  const [month, setMonth] = useState('')
-  const dispatch = useDispatch()
-  const submitHandler = (e) => {
-    e.preventDefault()
-    setValid(true)
-    setTimeout(() => {
-      setValid(false)
-    }, 10000)
-    dispatch(PaySalary(staffname.trim(), id, year, month, salary))
-    setStaffname('')
-    setId('')
-    setYear('')
-    setSalary('')
-    setMonth('')
-  }
-  const userLogin = useSelector((state) => state.userLogin)
+  const [selectedStaff, setSelectedStaff] = useState(null);
+  const [staffname, setStaffname] = useState('');
+  const [id, setId] = useState('');
+  const [valid, setValid] = useState(false);
+  const [year, setYear] = useState('');
+  const [salary, setSalary] = useState('');
+  const [month, setMonth] = useState('');
+
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
   // const userLogin = useSelector((state) => state.userLogin)
 
-  const { userCred } = userLogin
+  const { userCred } = userLogin;
 
   // const studentRegister = useSelector((state) => state.studentRegister)
-  const staffSalary = useSelector((state) => state.staffSalary)
+  const staffSalary = useSelector((state) => state.staffSalary);
+  const { loading: loadingSalary, success, error } = staffSalary;
 
-  const { loading, success, error } = staffSalary
+  const staffList = useSelector((state) => state.staffList);
+  const { loading: loadingStaff, staffs } = staffList;
+
+  const staffOptions = staffs.map(staff => ({ value: staff.staffId, label: staff.staff_name }));
+
   useEffect(() => {
-    dispatch({
-      type: STAFF_SALARY_RESET,
-    })
+    dispatch(listStaffs());
+    dispatch({ type: STAFF_SALARY_RESET });
     if (!userCred) {
       history.push('/login')
     }
-  }, [userCred, history])
+  }, [userCred, history]);
+
+  useEffect(() => {
+    if (selectedStaff) {
+      setId(selectedStaff.value);
+      setStaffname(selectedStaff.label);
+    }
+  }, [selectedStaff]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setValid(true);
+    setTimeout(() => {
+      setValid(false)
+    }, 10000);
+    dispatch(PaySalary(staffname.trim(), id, year, month, salary));
+    setStaffname('');
+    setId('');
+    setYear('');
+    setSalary('');
+    setMonth('');
+  };
+
+  const handleSelectChange = (selectedOption) => {
+    setSelectedStaff(selectedOption);
+    setId(selectedOption.value);
+    setStaffname(selectedOption.label);
+
+    const selectedStaffData = staffs.find(staff => staff.staffId === selectedOption.value);
+    if (selectedStaffData && selectedStaffData.estimated_salary) {
+      setSalary(selectedStaffData.estimated_salary.toString());
+    }
+  };
+
   return (
     <div className='container1' style={{ marginTop: '10px' }}>
       <div className='outer-layout'>
@@ -52,39 +82,42 @@ const StaffSalary = ({ history }) => {
         )}
         {valid && error && <Message variant='danger' message={error} />}
 
-        {loading ? (
+        {loadingStaff ? (
           <Loader />
         ) : (
           <form onSubmit={submitHandler}>
             <div className='form-inner'>
               <div className='form-control'>
-                <label for='name'>Staff Name</label>
-                <input
-                  type='text'
-                  value={staffname}
-                  onChange={(e) => setStaffname(e.target.value)}
-                  required
+                {/* Searchable Staff selection dropdown */}
+                <label htmlFor='staff'>Select Staff</label>
+                <Select
+                  options={staffOptions}
+                  onChange={handleSelectChange}
+                  value={selectedStaff}
+                  isSearchable
+                  placeholder="Select Staff"
                 />
               </div>
               <div className='form-control'>
                 <label for='name'>Staff ID</label>
-                <input
+                <Input
                   type='number'
                   value={id}
                   onChange={(e) => setId(e.target.value)}
+                  readOnly
                   required
                 />
               </div>
               <div className='form-control'>
                 <label for='year'>Salary for Year</label>
-                <input
+                <Input
                   type='string'
                   value={year}
                   onChange={(e) => setYear(e.target.value)}
                 />
-              </div>{' '}
+              </div>
               <div className='form-control'>
-                <label for='name'>Salary for Month</label>
+                <label for='month'>Salary for Month</label>
                 <select
                   id='class'
                   value={month}
@@ -107,23 +140,28 @@ const StaffSalary = ({ history }) => {
                   <option value='Chaitra'>Chaitra</option>
                   {/* <option value='Ten'>Ten</option> */}
                 </select>
-              </div>{' '}
+              </div>
               <div className='form-control'>
                 <label for='name'>Salary Amount</label>
-                <input
+                <Input
                   type='number'
                   value={salary}
                   onChange={(e) => setSalary(e.target.value)}
                   required
                 />
-              </div>{' '}
+              </div>
               {/* <div className="register-btn"> */}
               {/* </div> */}
             </div>
 
-            <button className='btn-register' type='submit'>
+            <Button
+              height={"auto"}
+              className="btn-register"
+              type="submit"
+              colorScheme="whatsapp"
+            >
               Pay Now
-            </button>
+            </Button>
           </form>
         )}
       </div>
@@ -131,4 +169,4 @@ const StaffSalary = ({ history }) => {
   )
 }
 
-export default StaffSalary
+export default StaffSalary;
